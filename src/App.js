47b61721492e0d1997298fa50cbc688ea01246cc
kallhiback <130337500+kallhiback@users.noreply.github.com>
2023-09-30 21:42:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [cards, setCards] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    const newCard = {
+      content: inputValue,
+    };
+    setCards([...cards, newCard]);
+    setInputValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="zam">
+      <div className="inputs">
+        <div className="input">
+          <input
+            placeholder="Добавить заметку.."
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button
+            onClick={handleButtonClick}
+            style={{
+              width: "50px",
+              height: "29px",
+              border: "2px solid rgb(0, 184, 190)",
+              borderLeft: "0px",
+              borderTopRightRadius: "20px",
+              borderBottomRightRadius: "20px",
+              backgroundColor: "white",
+              cursor: "pointer"
+            }}
+          >
+            Add
+          </button>
+        </div>  
+      </div>
+        <div className="cards">
+          {cards.map((card, index) => (
+            <div className="card" key={index}>
+              <p>{card.content}</p>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
-
-export default App;
